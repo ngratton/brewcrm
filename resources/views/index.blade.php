a2e0ee63501430bdd0779d1a6f4f2bd06dfc0765
Nicholas Gratton <nichgratton@gmail.com>
2020-02-13 21:38:56 -0500

    Ajustements HTML+CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,10 @@
     <div id="app">
         <nav id="main-nav">
             <div id="top-logo-ham">
-                <span class="logo-nav-open">brew CRM</span> <span class="hamburger">|||</span>
+                <span class="logo-nav-open">brew CRM</span>
+                <span class="hamburger">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+                </span>
             </div>
             <div class="nav-menu">
                 <h5>CONTACTS</h5>
@@ -58,8 +61,6 @@
             <contacts></contacts>
             <contacts></contacts>
             <contacts></contacts>
-
-            </div>
         </main>
     </div>
 </body>
